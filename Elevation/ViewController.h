9d4b3c3e11e786d501228dc27633f74eb0940737
Akash Mudubagilu <akashmudubagilu@gmail.com>
2016-03-31 20:13:38 -0600

    Initial Commit

---
+++ Elevation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Elevation
+//
+//  Created by Akash Mudubagilu on 3/31/16.
+//  Copyright © 2016 Akash Mudubagilu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Elevation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Elevation
+//
+//  Created by Akash Mudubagilu on 3/31/16.
+//  Copyright © 2016 Akash Mudubagilu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
